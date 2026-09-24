38591db5fd15fad3eafa7fd860f34a5df0da47f3
maulik-rastogi <rastogi.maulik30@gmail.com>
2026-09-23 21:42:43 -0700

    Revise welcome caption in governance guidance pack
    Updated the welcome caption text for clarity and consistency.

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ filtered_df = filtered_df.sort_values(
 st.title("Governance Guidance Pack: Artificial Intelligence (AI) in Education")
 
 st.caption(
-    "Welcome! This guidance pack is intended for the use of education stakeholders, such as educators, LEAs, and policymakers.
+    "Welcome! This guidance pack is intended for the use of education stakeholders, such as educators, LEAs, and policymakers."
    " Below, you will find a list of guidance documents, frameworks, and other governance resources. "
     "In each row, you can find items' basic information. In addition, GRAIL has also developed evaluations for each resource. "
     "For each column titled with a guidance topic, resources are scored (1-3) based on how deeply they cover that particular topic. Similarly, each column labelled with an underserved group evaluates how deeply the resource provides guidance for that underserved group specifically. Lastly, the final four columns evaluate each resource’s real-world usability, list the tools the resources offer, and provide notable information."
